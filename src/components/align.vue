6dc4e0eeed464b077d80125dfeecfcbe4711571e
syb <903923381@qq.com>
2023-02-11 15:05:54 +0800

    feat: 顶部-增加多选后垂直方法对齐功能

--- src/components/align.vue
+++ src/components/align.vue
@@ -21,6 +21,31 @@
         <path d="M944 1000h-64V24h64zM826 417H378V227h448zM826 797H80V607h746z" p-id="2544"></path>
       </svg>
     </Button>
+    <!-- 垂直对齐 -->
+    <Button :disabled="notMultiple()" @click="top">
+      <svg class="icon" height="14" p-id="1118" t="1650442692910" version="1.1"
+           viewBox="0 0 1170 1024" width="14" xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M1170.285714 36.571429a36.571429 36.571429 0 0 1-36.571428 36.571428H36.571429a36.571429 36.571429 0 0 1 0-73.142857h1097.142857a36.571429 36.571429 0 0 1 36.571428 36.571429z m-219.428571 146.285714v512a36.571429 36.571429 0 0 1-36.571429 36.571428h-219.428571a36.571429 36.571429 0 0 1-36.571429-36.571428v-512a36.571429 36.571429 0 0 1 36.571429-36.571429h219.428571a36.571429 36.571429 0 0 1 36.571429 36.571429z m-438.857143 0v804.571428a36.571429 36.571429 0 0 1-36.571429 36.571429h-219.428571a36.571429 36.571429 0 0 1-36.571429-36.571429v-804.571428a36.571429 36.571429 0 0 1 36.571429-36.571429h219.428571a36.571429 36.571429 0 0 1 36.571429 36.571429z"
+          fill="#666666" p-id="1119"></path>
+      </svg>
+    </Button>
+    <Button :disabled="notMultiple()" @click="ycenter">
+      <svg class="icon" height="14" p-id="1316" t="1650442732396" version="1.1"
+           viewBox="0 0 1243 1024" width="14" xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M548.571429 472.356571h146.285714V231.643429a36.571429 36.571429 0 0 1 36.571428-36.571429h219.428572a36.571429 36.571429 0 0 1 36.571428 36.571429v240.713142h179.785143a39.643429 39.643429 0 0 1 0 79.286858H987.428571v240.713142a36.571429 36.571429 0 0 1-36.571428 36.571429h-219.428572a36.571429 36.571429 0 0 1-36.571428-36.571429V551.643429h-146.285714V950.857143a36.571429 36.571429 0 0 1-36.571429 36.571428H292.571429a36.571429 36.571429 0 0 1-36.571429-36.571428V551.643429H76.214857a39.643429 39.643429 0 1 1 0-79.286858H256V73.142857A36.571429 36.571429 0 0 1 292.571429 36.571429h219.428571a36.571429 36.571429 0 0 1 36.571429 36.571428v399.213714z"
+          fill="#666666" p-id="1317"></path>
+      </svg>
+    </Button>
+    <Button :disabled="notMultiple()" @click="bottom">
+      <svg class="icon" height="14" p-id="920" t="1650442674784" version="1.1"
+           viewBox="0 0 1170 1024" width="14" xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M1170.285714 987.428571a36.571429 36.571429 0 0 0-36.571428-36.571428H36.571429a36.571429 36.571429 0 0 0 0 73.142857h1097.142857a36.571429 36.571429 0 0 0 36.571428-36.571429z m-219.428571-146.285714v-512a36.571429 36.571429 0 0 0-36.571429-36.571428h-219.428571a36.571429 36.571429 0 0 0-36.571429 36.571428v512a36.571429 36.571429 0 0 0 36.571429 36.571429h219.428571a36.571429 36.571429 0 0 0 36.571429-36.571429z m-438.857143 0V36.571429a36.571429 36.571429 0 0 0-36.571429-36.571429h-219.428571a36.571429 36.571429 0 0 0-36.571429 36.571429v804.571428a36.571429 36.571429 0 0 0 36.571429 36.571429h219.428571a36.571429 36.571429 0 0 0 36.571429-36.571429z"
+          fill="#666666" p-id="921"></path>
+      </svg>
+    </Button>
   </ButtonGroup>
 </template>
 
@@ -85,6 +110,51 @@ export default {
         })
       }
     },
+    // 顶部对齐
+    top () {
+      const activeObject = this.canvas.c.getActiveObject()
+      if (activeObject && activeObject.type === 'activeSelection') {
+        const activeSelection = activeObject
+        console.log(activeSelection)
+        const activeObjectTop = -(activeObject.height / 2)
+        activeSelection.forEachObject(item => {
+          item.set({
+            top: activeObjectTop,
+          })
+          item.setCoords()
+          this.canvas.c.renderAll()
+        })
+      }
+    },
+    // 底部对齐
+    bottom () {
+      const activeObject = this.canvas.c.getActiveObject()
+      if (activeObject && activeObject.type === 'activeSelection') {
+        const activeSelection = activeObject
+        const activeObjectTop = (activeObject.height / 2)
+        activeSelection.forEachObject(item => {
+          item.set({
+            top: activeObjectTop - (item.height * item.scaleY),
+          })
+          item.setCoords()
+          this.canvas.c.renderAll()
+        })
+      }
+    },
+    // 垂直居中对齐
+    ycenter () {
+      const activeObject = this.canvas.c.getActiveObject()
+      if (activeObject && activeObject.type === 'activeSelection') {
+        const activeSelection = activeObject
+        activeSelection.forEachObject(item => {
+          item.set({
+            top: 0 - ((item.height * item.scaleY) / 2),
+          })
+          item.setCoords()
+          this.canvas.c.renderAll()
+        })
+      }
+    }
   }
 }
 </script>
