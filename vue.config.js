2aea3c5012314601815d14303bb38cec84462341
syb <903923381@qq.com>
2023-03-22 09:50:08 +0800

    feat: 更改配置项

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,15 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue-fabric-editor/'
-    : '/',
+    ? './'
+    : './',
   transpileDependencies: true,
-  lintOnSave:false
-})
+  lintOnSave:false,
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      args[0].title = '设计编辑器'
+      return args
+    })
+  }
+});
